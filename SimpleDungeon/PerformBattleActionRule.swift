57e30e8b5f97f764515b2e3142d34911678e47a8
Todd Greener <todd.greener@gmail.com>
2015-12-19 14:10:00 -0700

    removed some files

--- SimpleDungeon/PerformBattleActionRule.swift
+++
@@ -1,9 +0,0 @@
-//
-//  PerformBattleActionRule.swift
-//  SimpleDungeon
-//
-//  Created by Todd Greener on 12/14/15.
-//  Copyright © 2015 Todd Greener. All rights reserved.
-//
-
-import Foundation
